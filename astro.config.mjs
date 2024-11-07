b58bcd4c6447a3210451f4da7bf535b0c719e0c2
Logan Bunch <loganbunch@gmail.com>
2024-11-06 21:27:30 -0800

    Wed Nov  6 21:27:30 PST 2024

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,58 @@ export default defineConfig({
             editLink: {
                 baseUrl: "https://github.com/logno-dev/class-notes/tree/main",
             },
+            sidebar: [
+                {
+                    label: "Get Started",
+                    link: "",
+                },
+                {
+                    label: "Lessons",
+                    items: [
+                        {
+                            label: "How to start coding",
+                            link: "lessons/how-to-start-coding",
+                        },
+                        {
+                            label: "Basic Javascript",
+                            link: "lessons/basic-javascript",
+                        },
+                        {
+                            label: "Loops",
+                            link: "lessons/loops",
+                        },
+                        {
+                            label: "Scope",
+                            link: "lessons/scope",
+                        },
+                        {
+                            label: "If Statements",
+                            link: "lessons/if",
+                        },
+                        {
+                            label: "Operators",
+                            link: "lessons/operators",
+                        },
+                        {
+                            label: "todo",
+                            link: "lessons/todo",
+                        },
+                        {
+                            label: "Path: Web Dev",
+                            items: [
+                                {
+                                    label: "Introduction",
+                                    link: "lessons/webdev/intro",
+                                },
+                                {
+                                    label: "HTML",
+                                    link: "lessons/webdev/html",
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
         }),
         sitemap(),
         mdx(),
---
+++ src/content/docs/Lessons/WebDev/intro.md
@@ -0,0 +1,12 @@
+---
+title: Introduction
+description: An introduction to the world of web development.
+sidebar:
+  order: 1
+---
+
+Learning coding with JavaScript is great for learning the basics, but what is the point of coding in general? Programs, at their core, take an input and provide an output. Part of a developers job is to handle that output, make it useful to someone. That could be as simple as displaying that output on a screen. Displaying output on a screen could be in the form of an application, which would need to be built specifically for each platform. These are called native apps and they require knowledge of system calls and tooling that is specific to each platform.
+
+Alternatively, we can build apps that run in a browser that can be accessed from any device. This is the beauty of web development. The web is cross-platform and extremely accessible. It is not without drawbacks of course, but that is beyond the point.
+
+In the following lessons we will learn the basics of HTML, CSS, and how to rope in some of the JavaScript we've learned to make some interactive websites.
